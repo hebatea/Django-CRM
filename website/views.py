abccd6c98b86d84cd10eae00393f1f80dc61ee1e
Heba Ashour <hashour2098@gmail.com>
2023-04-30 07:29:06 +0300

    Bootstarp Tables And Indiviual Records

--- website/views.py
+++ website/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+	records = Record.objects.all()
+
 	#check to see if logging in
 	if request.method == 'POST':
 		username = request.POST['username']
@@ -20,7 +23,7 @@ def home(request):
 			messages.success(request, "There was an error logging in. Please try again...")
 			return redirect('home')
 	else:
-		return render(request, 'home.html', {})
+		return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
 	logout(request)
@@ -44,4 +47,12 @@ def register_user(request):
 		return render(request, 'register.html', {'form': form})
 	return render(request, 'register.html', {'form':form})
 
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Lock up the record
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page")
+		return redirect('home')
 
